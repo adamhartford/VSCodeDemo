020139034e80f14c43ed3c8a6b3cd0990597e4b5
Adam Hartford <adam@adamhartford.com>
2018-01-26 09:04:07 -0500

    Update startServer.sh

--- startServer.sh
+++ startServer.sh
@@ -3,7 +3,7 @@ ps aux | grep -i xsp4.*5001 | xargs kill -9
 
 cd ./VSCodeDemo && MONO_MANAGED_WATCHER="y" MONO_OPTIONS="--debug --debugger-agent=transport=dt_socket,server=y,suspend=n,setpgid=y,address=127.0.0.1:55551"  MONO_ASPNET_WEBCONFIG_CACHESIZE="2000" xsp4 --address 127.0.0.1 --port 5001 --nonstop &
 
-# I've found that making a request too soon causes the debugger to fail to attach.
+# Wait to make sure the server has really started.
 sleep 2s
 
 open http://127.0.0.1:5001 &
