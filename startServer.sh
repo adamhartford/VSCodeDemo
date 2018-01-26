0822d6799887298a99e7e00f044db8a14c5d1c25
Adam Hartford <adam@adamhartford.com>
2018-01-26 13:59:49 -0500

    Update startServer.sh

--- startServer.sh
+++ startServer.sh
@@ -1,5 +1,5 @@
 # Stop any servers running on port 5001
-ps aux | grep -i xsp4.*5001 | xargs kill -9
+ps aux | grep -i xsp4.*5001 | awk '{print $2}' | xargs kill -9
 
 cd ./VSCodeDemo && MONO_MANAGED_WATCHER="y" MONO_OPTIONS="--debug --debugger-agent=transport=dt_socket,server=y,suspend=n,setpgid=y,address=127.0.0.1:55551"  MONO_ASPNET_WEBCONFIG_CACHESIZE="2000" xsp4 --address 127.0.0.1 --port 5001 --nonstop &
 
